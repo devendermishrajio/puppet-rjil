78a6921ab210dbfcee0c4bb8e6069e107a95caf5
Anshu Prateek <anshu.prateek@ril.com>
2015-11-06 11:18:09 +0530

    Adding more packages required for tools

--- manifests/commonservices/tools.pp
+++ manifests/commonservices/tools.pp
@@ -1,6 +1,6 @@
 # packages etc required for tools/commonservices
 class rjil::commonservices::tools {
-  $tools_pkgs = ['php5-snmp']
+  $tools_pkgs = ['php5-snmp', 'php5-gmp',]
 
   package { $tools_pkgs: }
 
